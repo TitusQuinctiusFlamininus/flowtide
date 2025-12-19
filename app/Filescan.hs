8c829b99dbbefa5ddf7c49736bef8491c383c97b
Ngumbah Michael Nyika <mnyika@gmail.com>
2025-12-19 11:28:58 +0100

    more edits
    - moved hibernate function to utils
    - exporting type synonyms

--- app/Filescan.hs
+++ app/Filescan.hs
@@ -5,9 +5,7 @@ import Utilities
 import Pipes
 import Data.Time.Clock
 import System.Directory
-import qualified Control.Concurrent as C
 
-type SleepCount   = Int
 type TDDFile      = FilePath
 
 type NewContent   = [Char]
@@ -20,8 +18,9 @@ scan sleep tfile = do
     modTime    <- lift $ getModificationTime tfile    
     oldModTime <- lift $ readLastRecordedModTime tfile
     case (diffUTCTime modTime oldModTime) of
-        0  -> (lift $ C.threadDelay (sleep*1000000)) >> (scan sleep tfile)
-        _  -> do 
-                newContent <- lift $ readFile tfile
-                oldContent <- lift $ readFileInDatabase tfile
-                yield (newContent, oldContent)
+        0.0  -> (lift $ hibernate sleep) >> (scan sleep tfile)
+        _    -> do 
+                    newContent <- lift $ readFile tfile
+                    oldContent <- lift $ readFileInDatabase tfile
+                    yield (newContent, oldContent)
+                    (lift $ hibernate sleep) >> (scan sleep tfile)
--- app/Utilities.hs
+++ app/Utilities.hs
@@ -1,14 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Utilities (readLastRecordedModTime,
-                  readFileInDatabase) where
+                  readFileInDatabase,
+                  hibernate, 
+                  SleepCount) where
 
 import Data.Time.Clock
 import System.Directory
 import Database.PostgreSQL.Simple
+import qualified Control.Concurrent as C
+
+
+-- TYPE SYNONYMS
+type SleepCount   = Int
+
 
 readLastRecordedModTime :: FilePath -> IO UTCTime
 readLastRecordedModTime = undefined
+
+-- TIME UTILS --
+-- General Sleeper Function 
+hibernate ::  SleepCount -> IO ()
+hibernate sleep = C.threadDelay (sleep*1000000)
+
 
 
 -- DATABASE CONNECTION UTILS --
